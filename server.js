e1f0df22495a3c2a7ac76657b742982469c88c00
avocados23 <namht@vt.edu>
2023-03-02 08:49:46 -0500

    connects to mongodb instance

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const express = require('express');
-const { PythonShell } = require('python-shell')
+const { PythonShell } = require('python-shell');
 const app = express();
 const port = process.env.PORT || 3002;
-
-app.set('view engine', 'ejs');
+const mongoose = require('mongoose');
+const URI = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPWD}@cluster0.iz0pkfv.mongodb.net/?retryWrites=true&w=majority`;
 
 app.route('/').get((_, res) => {
     res.render('index');
@@ -20,3 +21,13 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(port, () => console.log('Server started on', port));
+
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully');
+});
