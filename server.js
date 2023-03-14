9cf39dd742a583a6adba1397badf51a87b870ee8
avocados23 <namht@vt.edu>
2023-03-14 08:26:23 -0400

    procfile modified again

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use((err, req, res, next) => {
 
 app.set('view engine', 'ejs');
 app.listen(port, () => {
-    console.log(`Server is listening on http://localhost:${port}`);
+    console.log(`Server is listening on ${port}`);
 });
 
 mongoose.connect(URI, {
