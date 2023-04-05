ffe69cff149e1d0a2a0e63479d643b39db5d1269
jangwool <jangwool@vt.edu>
2023-04-04 20:06:12 -0400

    extend dataframe using random number of minutes

--- forecast.py
+++ forecast.py
@@ -3,6 +3,9 @@ import csv
 import pandas as pd
 from prophet import Prophet
 
+import random
+import datetime
+
 # Sign1_full_fitted.csv
 df = pd.read_csv('Sign1_full_fitted.csv')
 
@@ -12,10 +15,12 @@ df = df.rename(columns={'ts': 'ds', 'y1': 'y'})
 p = Prophet()
 p.fit(df)
 
-future = p.make_future_dataframe(periods=365)
+# random number of minutes (1 - 30)
+minutes = random.randint(1, 30)
+
+future = p.make_future_dataframe(periods=minutes, freq='min')
 
 forecast = p.predict(future)
-#forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
 
 forecast[['ds', 'yhat']].to_csv('forecast1.csv', index=False)
 
@@ -29,7 +34,7 @@ df2 = df2.rename(columns={'ts': 'ds', 'y12': 'y'})
 p2 = Prophet()
 p2.fit(df2)
 
-future2 = p2.make_future_dataframe(periods=365)
+future2 = p2.make_future_dataframe(periods=minutes, freq='min')
 
 forecast2 = p2.predict(future2)
 #forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
@@ -46,7 +51,7 @@ df3 = df3.rename(columns={'ts': 'ds', 'y14': 'y'})
 p3 = Prophet()
 p3.fit(df3)
 
-future3 = p3.make_future_dataframe(periods=365)
+future3 = p3.make_future_dataframe(periods=minutes, freq='min')
 
 forecast3 = p3.predict(future3)
 #forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
