859cbac60b61ae6b67c61117bddf57385945721c
avocados23 <namht@vt.edu>
2023-04-17 02:10:50 -0400

    removed all python files, added endpoint that gets actual

--- parse.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import csv
-
-with open('Sign12_full_fitted.csv', 'r') as csv_file:
-    reader = csv.reader(csv_file)
-
-    for row in reader:
-        print(row)
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const express = require('express');
-const { PythonShell } = require('python-shell');
 const app = express();
 const mongoose = require('mongoose');
+const xmlParser = require('xml2json');
 
 const basicLib = require('./lib/basic');
 const checkValidLatLong = require('./utils/checkValidLatLong');
@@ -10,47 +10,8 @@ const checkValidLatLong = require('./utils/checkValidLatLong');
 const port = process.env.PORT || 3000;
 const URI = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPWD}@cluster0.iz0pkfv.mongodb.net/?retryWrites=true&w=majority`;
 
-const { spawn } = require('child_process');
-
 app.route('/').get((_, res) => {
     res.render('index');
-});
-
-app.get('/sign12', async (_, res) => {
-    const data = await PythonShell.run('parse.py', null);
-    return res.send(data);
-});
-
-app.get('/forecast1', async (_, res) => {
-    const data = await basicLib.readCsv('forecast1.csv');
-    return res.status(200).send(data);
-});
-
-app.get('/forecast12', async (_, res) => {
-    const data = await basicLib.readCsv('forecast12.csv');
-    return res.status(200).send(data);
-});
-
-app.get('/forecast14', async (_, res) => {
-    const data = await basicLib.readCsv('forecast14.csv');
-    return res.status(200).send(data);
-});
-
-app.get('/forecast', async (_, res) => {
-    const script = spawn('python', ['forecast.py']);
-  
-    script.stdout.on('data', (data) => {
-        res.send(data.toString());
-    });
-  
-    script.stderr.on('data', (data) => {
-        console.error(data.toString());
-        res.status(500).send('Error executing forecast.py');
-    });
-  
-    script.on('close', (code) => {
-        console.log(`Python script exited with code ${code}`);
-    });
 });
 
 app.get('/getParkingGarage', async (_, res) => {
@@ -74,6 +35,12 @@ app.get('/predict', checkValidLatLong, async (req, res) => {
 
     const data = await basicLib.getFlaskServerSpots(lat, long);
     return res.send(data);
+});
+
+app.get('/actual', async (_, res) => {
+    const data = await basicLib.getActualSpots();
+    let xmlToJSON = JSON.parse(xmlParser.toJson(data));
+    return res.send(xmlToJSON);
 });
 
 app.get('/testRoute', async (_, res) => {
